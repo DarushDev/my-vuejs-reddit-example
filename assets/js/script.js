d9d2aa0ef31ae9029fc96ded8ec4b57b9013621e
DarushDev <darushfaqiri@gmail.com>
2017-11-11 14:17:42 +0800

    Create subreddit component

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Parent | Subreddit component containing a list of 'post' components.
+var subreddit = Vue.component('subreddit', {
+  template: '#subreddit',
+  props: ['name'],
+  data: function () {
+    return {
+      posts: []
+    }
+  },
+  created: function () {
+    this.$http.get("https://www.reddit.com/r/" + this.name + "/top.json?limit=5")
+      .then(function (resp) {
+        if(typeof reps.data === 'string') {
+          reps.data = JSON.parse(resp.data);
+        }
+        this.posts = resp.data.data.children;
+      })
+  }
+});
+
+new Vue({
+  el: "#main"
+});
